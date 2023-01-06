9aef37cfbd6e1e839b18f713679fde3c3ebdea1d
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-01-06 16:34:31 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Sum
+{
+    public int GetSum(int a, int b)
+    {
+        int max = Math.Max(a, b);
+        int min = Math.Min(a, b);
+        int result = 0;
+        for (int i = min; i <= max; i++)
+        {
+            result += i;
+        }
+        return result;
+    }
+}
